c3a145a06b912b1d3317e3fb2f0f5beb6aee476c
arthurtemporim <arthurrtl@gmail.com>
2021-09-25 12:47:02 -0300

    Start configuration script of Arch Linux env
    Signed-off-by: arthurtemporim <arthurrtl@gmail.com>

---
+++ arch/install.sh
@@ -0,0 +1,24 @@
+# Install
+
+## System
+sudo pacman -S --needed base-devel openssl zlib xz
+yay -S vim git links htop terminator
+git config --global user.name arthurtemporim
+git config --global user.email arthurrtl@gmail.com
+ssh-keygen
+
+## Python
+yay -S pyenv pyenv-virtualenvwrapper
+echo $PATH | grep --color=auto "$(pyenv root)/shims"
+pyenv install 3.7.9
+pyenv global 3.7.9
+pip install --upgrade pip
+pip install virtualenvwrapper
+
+
+# Docker
+yay -S docker docker-compose
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo groupadd docker
+newgrp docker 
