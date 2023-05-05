be4efd453945426e799389287ac594c644624836
Arthur Temporim <arthurrtl@gmail.com>
2023-05-05 15:27:47 -0300

    Add rasp dir
    Signed-off-by: Arthur Temporim <arthurrtl@gmail.com>

---
+++ rasp/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Update and install core software
+sudo apt update && sudo apt upgrade -y && sudo apt install vim git links htop tree -y && sudo reboot
+# Docker
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo bash get-docker.sh && sudo usermod -aG docker $(whoami) && sudo systemctl enable docker && sudo systemctl start docker
+# OMV
+# wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
+# Connect to keyboard
+# bluetoothctl agent on && bluetoothctl pair 08:21:01:16:05:89 && bluetoothctl connect 08:21:01:16:05:89
