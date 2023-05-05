4853f89ba2c559967ce26940bb2ae740908894e9
Arthur Temporim <arthurrtl@gmail.com>
2023-05-05 15:34:54 -0300

    Add pihole command

--- rasp/install.sh
+++ rasp/install.sh
@@ -7,3 +7,5 @@ curl -fsSL https://get.docker.com -o get-docker.sh && sudo bash get-docker.sh &&
 # wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
 # Connect to keyboard
 # bluetoothctl agent on && bluetoothctl pair 08:21:01:16:05:89 && bluetoothctl connect 08:21:01:16:05:89
+# PiHole
+curl -sSL https://install.pi-hole.net | bash
