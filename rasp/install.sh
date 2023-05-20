0cd1d324672d4355c936950cb4ee7da74c7b31df
Arthur Temporim <arthurrtl@gmail.com>
2023-05-19 23:19:28 -0300

    Add rasp scripts
    Signed-off-by: Arthur Temporim <arthurrtl@gmail.com>

--- rasp/install.sh
+++ rasp/install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # Update and install core software
+echo "Updating system"
 sudo apt update && sudo apt upgrade -y && sudo apt install vim git links htop tree -y && sudo reboot
 # Docker
+echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo bash get-docker.sh && sudo usermod -aG docker $(whoami) && sudo systemctl enable docker && sudo systemctl start docker
 # OMV
-# wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
+echo "Installing Open Meedia Vault"
+wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
+# Home Assistant
+echo "Installing Home Assistant"
+sudo mkdir -p ~/.config/lxsession/LXDE-pi
+sudo echo -e "\n\
+@lxpanel --profile LXDE-pi\n\
+@pcmanfm --desktop --profile LXDE-pi\n\
+#@xscreensaver -no-splash\n\
+point-rpi\n\
+@chromium-browser --start-fullscreen --start-maximized http://localhost:8123\n\
+" >> ~/.config/lxsession/LXDE-pi/autostart
+#
 # Connect to keyboard
 # bluetoothctl agent on && bluetoothctl pair 08:21:01:16:05:89 && bluetoothctl connect 08:21:01:16:05:89
---
+++ rasp/network.sh
@@ -0,0 +1,9 @@
+sudo rm -Rf /var/run/wpa_supplicant/wlan0
+sudo echo -e "\n\
+network={\n\
+	ssid=\"network\"\n\
+	psk=\"password\"\n\
+}\n\
+" >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf -D nl80211
+sudo dhclient wlan0
