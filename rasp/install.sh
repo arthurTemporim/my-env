ac78b3fc9305c4d2614ae867553ecb52ec1e61d2
Arthur Temporim <arthurrtl@gmail.com>
2023-05-21 11:38:47 -0300

    Update install.sh

--- rasp/install.sh
+++ rasp/install.sh
@@ -8,11 +8,5 @@ curl -fsSL https://get.docker.com -o get-docker.sh && sudo bash get-docker.sh &&
 # OMV
 echo "Installing Open Meedia Vault"
 wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
-# Home Assistant
-echo "Installing Home Assistant"
-cd && cd Documents
-docker compose up -d
-cd
-#
 # Connect to keyboard
 # bluetoothctl agent on && bluetoothctl pair 08:21:01:16:05:89 && bluetoothctl connect 08:21:01:16:05:89
